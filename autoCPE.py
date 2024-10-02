fc00ae96f198faaac5ad57765feef3724d5468b1
x96 <a@a.a>
2024-10-02 09:33:36 +0800

    yeah

--- autoCPE.py
+++ autoCPE.py
@@ -3,7 +3,7 @@ from playwright.sync_api import Playwright, sync_playwright, expect
 
 
 def run(playwright: Playwright) -> None:
-    browser = playwright.chromium.launch(headless=False,slow_mo=2000)
+    browser = playwright.chromium.launch(headless=True,slow_mo=2000)
     context = browser.new_context()
     page = context.new_page()
     page.goto("http://192.168.1.1/common/login.html")
@@ -12,11 +12,12 @@ def run(playwright: Playwright) -> None:
     page.locator("#password").click()
     page.locator("#password").fill("admin")
     page.get_by_role("button", name="登录").click()
+    print('Login Succ!')
     page.locator("span").filter(has_text="系统").first.click()
     page.get_by_role("link", name="重启 & 恢复出厂设置").click()
     page.get_by_role("button", name="重启").click()
     page.get_by_text("确定", exact=True).click()
-
+    print('Done!!')
     # ---------------------
     context.close()
     browser.close()
