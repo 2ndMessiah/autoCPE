a5df4f19d6560ad10f6a2fcf50bf1bf8c48c5289
Preston <>
2024-11-10 08:00:57 +0800

    no message

--- yacd.py
+++ yacd.py
@@ -5,11 +5,15 @@ import sys
 
 
 yacdProxyPath="http://192.168.31.1:9999/ui/#/proxies"
-def run(playwright: Playwright,proxy,proxyemby) -> None:
+def run(playwright: Playwright,proxy,proxyemby,autoclose) -> None:
     browser = playwright.chromium.launch(headless=True)
     context = browser.new_context()
     page = context.new_page()
     page.goto(yacdProxyPath)
+    if autoclose:
+        page.locator("._btn_vsco8_4").first.click()
+        page.locator("div").filter(has_text=re.compile(r"^切换代理时自动断开旧连接$")).locator("div").nth(3).click()
+        page.get_by_role("dialog").press("Escape")
     page.get_by_role("button", name="科学上网Selector").click()
     page.get_by_text(proxy).first.click()
     page.get_by_role("button", name="emby_selSelector").click()
@@ -22,12 +26,13 @@ def run(playwright: Playwright,proxy,proxyemby) -> None:
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print("Usage: python script.py <yacd_proxy_path> <selector_name>")
+    if len(sys.argv) != 4:
+        print("Usage: python script.py <yacd_proxy_path> <selector_name> <autoclose:1/0>")
         sys.exit(1)
     
     proxy = sys.argv[1]
     proxyemby = sys.argv[2]
+    autoclose = sys.argv[3]
 
     with sync_playwright() as playwright:
         run(playwright, proxy, proxyemby)
